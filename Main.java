ee4e0854f6d07d85123c12d0b0b96af8ca583a09
ademiltonbarbos <105940912+ademiltonbarbosa@users.noreply.github.com>
2022-06-04 13:50:45 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,23 @@
+import javax.swing.JOptionPane;
+class Main {
+  public static void main(String[] args) {
+float a,b;
+
+a=Float.parseFloat(JOptionPane.showInputDialog("DIGITE UM NUMERO"));
+
+b=Float.parseFloat(JOptionPane.showInputDialog("DIGITE UM NUMERO"));
+
+float c= a+b;   
+JOptionPane.showMessageDialog(null,"O VALOR DA OPERAÇÃO E: "+c);
+ 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+}
